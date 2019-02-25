7d132a631d0314d9fa3853b2499f85902f902858
Matt <cook6348@gmail.com>
2019-02-25 08:25:54 -0600

    making custom component and rendering it on the hompage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //Assigns a string as a value, and its value it app-root. This is the information angular needed in the index.html
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
